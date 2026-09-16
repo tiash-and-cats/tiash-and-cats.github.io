6d7cfe82221143284ba77d3281e0b1a4a74d7f76
tiash-and-cats <tiash.rbm@gmail.com>
2026-09-16 12:58:55 +0600

    Added relief for weak computers

--- vendor.sh
+++ vendor.sh
@@ -1,9 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
-echo "===== three.js ================================"
-mkdir -p assets/vendor/threejs
-curl https://registry.npmjs.org/three/-/three-0.186.0.tgz | tar -vxz -C assets/vendor/threejs --strip-components=1 --exclude="package/src" package
-echo "Please do not worry if it says 'package: not found in archive'."
+npmvendor() {
+    local name=$1
+    local version="${2:-$(npm show "$name" version)}"
+    local dir="${3:-assets/vendor/$name}"
+    local tarargs="${4:-}"
+    
+    echo "===== $name"
+
+    local urlname=$(echo "$name" | sed 's/\//%2F/g')
+    local pkgname=$(basename "$name")
+
+    mkdir -p "$dir"
+    curl -sLf "https://registry.npmjs.org/$urlname/-/$pkgname-$version.tgz" \
+      | (tar -vxz -C "$dir" --strip-components=1 package $tarargs || true)
+    echo "Please do not worry if it says 'package: not found in archive'."
+}
+
+npmvendor three 0.186.0 assets/vendor/threejs "--exclude=package/src"
+npmvendor @pmndrs/detect-gpu 6.0.22
 
 echo
 echo "done!"
